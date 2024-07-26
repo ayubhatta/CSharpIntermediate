aa1471a0273e751d5acab4fe2ea80d2cae786253
Ayub Bhatta <bhatta.ayub1263@gmail.com>
2024-07-26 12:57:24 +0545

    Add project files.

---
+++ CSharpIntermediate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpIntermediate
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+            customer.Id = 1;
+            customer.Name = "John";
+
+            var order = new Order();
+            customer.Orders = new List<Order>();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
